6d039968684ca595b94f4a8c926b56746fe34a4a
hasnatulhaq <hasnatulhaq@outlook.com>
2022-09-21 17:34:57 +0500

    final design

--- src/App.js
+++ src/App.js
@@ -47,17 +47,17 @@ function App() {
          <MapsGoogle/>
         <div id="legend" className='bg-white w-52 border-[1px] z-[300] h-80 ml-16 mt-[500px] flex flex-col border-lightgray rounded-md text-center bottom-5 absolute '>
           {/* <div className='border-[2px] rounded-full border-zblue w-8 h-8 absolute top-[-8px] right-[-8px] bg-purple'> */}
-            <div className='flex justify-center items-center border-zblue bg-white border-[1px] w-8 h-8 rounded-full self-end top-[-10px] right-[-10px] absolute'>
+            <div className='flex justify-center items-center border-zblue bg-white border-[1px] w-8 h-8 rounded-full top-[-10px] right-[-10px] absolute'>
           <img src={Colorpicker} alt="color picker" className='h-4'/>
             </div>
           {/* </div> */}
         {/* <img src={Colorpicker} alt="color picker" className='rounded border-[1px] border-zblue w-5 self-end absolute'/> */}
-           <div className='border-b-[1px] border-lightgray font-normal flex flex-row justify-center p-2'>
-              <div>Zone Legend</div> 
+           <div className='border-b-[1px] border-lightgray font-normal flex flex-row justify-center p-2 uppercase text-zsm font-medium'>
+              Zone Legend
            </div>
            <div className='text-left font-normal overflow-auto'>
                <ul>
-                   <li><div className='w-4 rounded h-2 bg-purple'></div>code</li>
+                   <li className='flex gap-2'><div className='w-4 rounded-full h-4 bg-purple'></div><div>code</div></li>
                    <li className='flex flex-row'><span className='bg-gray rounded'></span><span>code</span></li>
                    <li><span className='bg-gray rounded'></span>code</li>
                    <li><span className='bg-gray rounded'></span>code</li>
