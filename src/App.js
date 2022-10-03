0225aa7c026ef3b0acda0b4bb04ab63407dedfaa
Athar Sajjad <mac@Macs-MacBook-Pro.local>
2022-10-03 15:04:04 +0500

    style(App): Update the styles for pop up

--- src/App.js
+++ src/App.js
@@ -1,97 +1,109 @@
-import './App.css';
-import '../src/global.css'
-import {FaFilter,FaBookmark, FaMapPin} from 'react-icons/fa';
-import MapsGoogle from './Components/Googlemap/MapsGoogle';
-import {Autocomplete} from '@react-google-maps/api';
-import Button from './Components/Button/Button';
-import Navbar from './Components/Navbar/Navbar';
-import RightSidebar from './Components/RightSidebar/Rightsidebar';
-import {ReactComponent as Bookmark}   from './bookmark.svg'
-import {ReactComponent as Pin} from './images/pin-o.svg'
-import {ReactComponent as Filter} from './images/o-filter.svg';
-import {ReactComponent as Layer} from './images/o-layer.svg'
-import {ReactComponent as Opacity} from './images/o-brightness.svg'
-import {ReactComponent as Screenshot} from './images/o-screenshot.svg'
-import {ReactComponent as Print} from './images/o-print.svg'
-import {ReactComponent as Zoingboard} from './images/o-zoingboard.svg'
-import Legend from './Components/Legend/Legend';
-
-
+import "./App.css";
+import "../src/global.css";
+import { FaFilter, FaBookmark, FaMapPin } from "react-icons/fa";
+import MapsGoogle from "./Components/Googlemap/MapsGoogle";
+import { Autocomplete } from "@react-google-maps/api";
+import Button from "./Components/Button/Button";
+import Navbar from "./Components/Navbar/Navbar";
+import RightSidebar from "./Components/RightSidebar/Rightsidebar";
+import { ReactComponent as Bookmark } from "./bookmark.svg";
+import { ReactComponent as Pin } from "./images/pin-o.svg";
+import { ReactComponent as Filter } from "./images/o-filter.svg";
+import { ReactComponent as Layer } from "./images/o-layer.svg";
+import { ReactComponent as Opacity } from "./images/o-brightness.svg";
+import { ReactComponent as Screenshot } from "./images/o-screenshot.svg";
+import { ReactComponent as Print } from "./images/o-print.svg";
+import { ReactComponent as Zoingboard } from "./images/o-zoingboard.svg";
+import Legend from "./Components/Legend/Legend";
 
 function App() {
-
   return (
     <>
-       <Navbar/>
-    {/* search menu  */}
-      <div className='w-11/12 lg:h-full bg-white p-3 flex gap-5 lg:flex-row md:flex-col sm:flex-col sm:h-24'>
-           <div className='border-[1px] border-zlightgray lg:w-2/6 h-11 ml-5 rounded-lg p-2 flex flex-row gap-3 shadow-sm px-6 md:w-full'>
-            <button className='px-2'>
-                <Bookmark className="fill-zlightgray h-6 w-6"/>
-            </button>
-            <span className='border-l-[1px] border-zlightgray'></span>
-            <div className='self-center'>
+      <Navbar />
+      {/* search menu  */}
+      <div className="w-11/12 lg:h-full bg-white p-3 flex gap-5 lg:flex-row md:flex-col sm:flex-col sm:h-24">
+        <div className="border-[1px] border-zlightgray lg:w-2/6 h-11 ml-5 rounded-lg p-2 flex flex-row gap-3 shadow-sm px-6 md:w-full">
+          <button className="px-2">
+            <Bookmark className="fill-zlightgray h-6 w-6 mx-auto my-auto" />
+          </button>
+          <span className="border-l-[1px] border-zlightgray"></span>
+          <div className="self-center">
             <Autocomplete
-      // onLoad={(e)=>setAutocomplete(e)}
-      //  onPlaceChanged={onPlaceChanged}
-       restrictions={{country: "us"}}>
-        <input id='place-id' type='text' placeholder='Search address' className='focus:outline-none'/>
-      </Autocomplete>
-            </div>
-           </div>
-           <div className='flex gap-4 lg:flex-row items-end'>
-           <Button text={"Zone Picker"} icon={<Filter  className="w-3 h-2.5 fill-gray"/>}/>
-           <Button text={"Plu Filter"} icon={<Filter  className="w-3 h-2.5 fill-gray"/>}/>
-           <Button text={"Prospect"} icon={<Filter  className="w-3 h-2.5 fill-gray"/>}/>
-           <Button text={"Zone Cases"} icon={<Pin className="w-2.5 h-4 fill-gray"/>}/>
-           <Button text={"Listings"} icon={<Pin className="w-2.5 h-4 fill-gray"/>}/>
-           </div>
+              // onLoad={(e)=>setAutocomplete(e)}
+              //  onPlaceChanged={onPlaceChanged}
+              restrictions={{ country: "us" }}
+            >
+              <input
+                id="place-id"
+                type="text"
+                placeholder="Search for zoning data"
+                className="focus:outline-none"
+              />
+            </Autocomplete>
+          </div>
+        </div>
+        <div className="flex gap-4 lg:flex-row items-end">
+          <Button
+            text={"Zone Picker"}
+            icon={<Filter className="w-3 h-2.5 fill-gray" />}
+          />
+          <Button
+            text={"Plu Filter"}
+            icon={<Filter className="w-3 h-2.5 fill-gray" />}
+          />
+          <Button
+            text={"Prospect"}
+            icon={<Filter className="w-3 h-2.5 fill-gray" />}
+          />
+          <Button
+            text={"Zone Cases"}
+            icon={<Pin className="w-2.5 h-4 fill-gray" />}
+          />
+          <Button
+            text={"Listings"}
+            icon={<Pin className="w-2.5 h-4 fill-gray" />}
+          />
+        </div>
       </div>
-      <div className='items-center w-full'>
-      <div className='z-20 mx-6 my-1 w-1/5 absolute h-35 flex gap-10 p-2 bg-ztranparent'>
-            <div className='flex flex-col justify-center h-full w-10 items-center bg-zlightgray rounded'>
-              <div className=' bg-zmapmenu flex flex-col w-full h-full rounded justify-center items-center text-white'>
-              <span className='text-xl text-zstrip items-center'>+</span>
-              <span className='border-b-[1px] w-full border-zlightgray '></span>
-              <span className='text-xl items-center'>-</span>
-              </div>
+      <div className="items-center w-full relative">
+        <div className="z-20 mx-6 my-1 w-1/5 absolute h-35 flex gap-10 p-2 bg-ztranparent">
+          <div className="flex flex-col justify-center h-full w-10 items-center bg-zlightgray rounded">
+            <div className=" bg-zmapmenu flex flex-col w-full h-full rounded justify-center items-center text-white">
+              <span className="text-xl text-zstrip items-center">+</span>
+              <span className="border-b-[1px] w-full border-zlightgray "></span>
+              <span className="text-xl items-center">-</span>
             </div>
-            <div className='w-full flex flex-row gap-[1px] h-[35px] justify-center items-center bg-zlightgray rounded-md text-white'>
-                  <div className='w-1/5 h-full rounded-l-md bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]'><Layer className='fill-white'/></div>
-                  <div className='w-1/5 h-full bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]'><Opacity className='fill-white'/></div>
-                  <div className='w-1/5 h-full bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]'><Screenshot className='fill-white'/></div>
-                  <div className='w-1/5 h-full bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]'><Print className='fill-white'/></div>
-                  <div className='w-1/5 h-full bg-zmapmenu rounded-r-md flex justify-center items-center pt-[0.5rem] pb-[0.5rem]'><Zoingboard className="fill-white stroke-1 stroke-gray"/></div>
+          </div>
+          <div className="w-full flex flex-row gap-[1px] h-[35px] justify-center items-center bg-zlightgray rounded-md text-white">
+            <div className="w-1/5 h-full rounded-l-md bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]">
+              <Layer className="fill-white" />
             </div>
+            <div className="w-1/5 h-full bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]">
+              <Opacity className="fill-white" />
+            </div>
+            <div className="w-1/5 h-full bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]">
+              <Screenshot className="fill-white" />
+            </div>
+            <div className="w-1/5 h-full bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]">
+              <Print className="fill-white" />
+            </div>
+            <div className="w-1/5 h-full bg-zmapmenu rounded-r-md flex justify-center items-center pt-[0.5rem] pb-[0.5rem]">
+              <Zoingboard className="fill-white stroke-1 stroke-gray" />
+            </div>
+          </div>
+        </div>
+        <MapsGoogle />
+        <Legend />
+        <RightSidebar />
       </div>
-         <MapsGoogle/>
-         <Legend/>
-      </div>
-      <RightSidebar/>
     </>
   );
 }
 
-
 export default App;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <div id="legend" className='bg-white w-56 border-[1px] z-[300] h-96 ml-16 mt-[500px] flex flex-col border-zlightgray rounded-[14px] text-center bottom-5 absolute '>
+{
+  /* <div id="legend" className='bg-white w-56 border-[1px] z-[300] h-96 ml-16 mt-[500px] flex flex-col border-zlightgray rounded-[14px] text-center bottom-5 absolute '>
             <div className='flex justify-center items-center border-zblue bg-white border-[2px] w-9 h-9 rounded-full top-[-10px] right-[-10px] absolute'>
                  <Colorpicker className="fill-zblue h-4"/>
             </div>
@@ -120,14 +132,9 @@ export default App;
                    <li className='flex gap-2 items-center'><div className='w-3 rounded-full h-3 bg-purple'></div><div>code</div></li>
                </ul>
            </div>
-          </div> */}
+          </div> */
+}
 
-
-
-
-
-
-
-  // const style = { color: "lightgray", fontSize: "1.5em" }
-  // const filterstyle = { color : "lightgray"}
-  // const mappin={colo:"gray"}
+// const style = { color: "lightgray", fontSize: "1.5em" }
+// const filterstyle = { color : "lightgray"}
+// const mappin={colo:"gray"}
--- src/Components/RightSidebar/Rightsidebar.js
+++ src/Components/RightSidebar/Rightsidebar.js
@@ -1,63 +1,77 @@
-import Rightbarbtn from '../Rightbarbtn/Rightbarbtn';
-import Rightbartab from '../Rightbartab/Rightbartab';
-import {FaFileAlt, FaUndo, FaDownload,FaLink, FaBookmark, FaCross, FaTimes} from 'react-icons/fa';
-import {ReactComponent as BookmarkIcon} from '../../images/bookmark.svg'
-import {ReactComponent as Newtab} from '../../images/Newtab-s.svg'
-import {ReactComponent as Bugreport} from '../../images/bug-report.svg'
-import {ReactComponent as Cross} from '../../images/cross.svg'
+import Rightbarbtn from "../Rightbarbtn/Rightbarbtn";
+import Rightbartab from "../Rightbartab/Rightbartab";
+import {
+  FaFileAlt,
+  FaUndo,
+  FaDownload,
+  FaLink,
+  FaBookmark,
+  FaCross,
+  FaTimes,
+} from "react-icons/fa";
+import { ReactComponent as BookmarkIcon } from "../../images/bookmark.svg";
+import { ReactComponent as Newtab } from "../../images/Newtab-s.svg";
+import { ReactComponent as Bugreport } from "../../images/bug-report.svg";
+import { ReactComponent as Cross } from "../../images/cross.svg";
 
+function RightSidebar() {
+  const style = { color: "lightgray", fontSize: "2em" };
+  const btnstyle = { fontSize: "1.0rem" };
 
-function RightSidebar(){
-
-
-    const style = { color: "lightgray", fontSize: "2em" }
-    const btnstyle ={ fontSize: "1.0rem"}
-
-    return(
-          <div className=' bg-white w-[32%] h-[87%] z-auto absolute right-0 bottom-0 lg:block md:hidden sm:hidden'>
-          <div className='flex flex-row border-b-[1px] border-zlightgray justify-between items-center p-2 h-9'>
-            <span className='text-purple'><Newtab className="fill-purple h-3.5 w-3.5"/></span>
-            <div className='flex flex-row'>
-            <Bugreport className="h-5 w-5"/>
-            <span className='text-gray font-light text-base'>
-              <Cross className="h-5 w-5"/>
-              </span>
-            </div>
-          </div>
-          <div className='flex justify-between border-b-[1px] border-zlightgray px-5 pr-5'>
-            <h1 className='text-xl text-zblue h-20 p-2 font-medium'>Los Angeles,CA,USA</h1>
-            <span className='mt-5 mr-5'>
-              <BookmarkIcon className="w-10 h-10 fill-zlightgray"/>
-            </span>
-          </div>
-          <div className='flex w-full border-b-[1px] border-zlightgray p-4'>
-            <Rightbartab text={"Zone Data"}/>
-            <Rightbartab text={"Permitted use"}/>
-            <Rightbartab text={"Controls"}/>
-            <Rightbartab text={"Overlays"}/>
-          </div>
-          <div className='h-[500px] overflow-auto p-5'>
-              <ul className='leading-10 uppercase font-medium text-xs'>
-                <li>Zone Code</li>
-                  <li className='text-gray'>C35</li>
-                <li>Zone name</li>
-                <ul>
-                  <li className='text-gray'>C4</li>
-                </ul>
-                <li>Zone type</li>
-                <ul>
-                  <li className='text-gray'>Comercial</li>
-                </ul>   
-              </ul>
-          </div>
-          <div className='flex p-4 gap-1 bottom-0 absolute w-full border-t-[1px] border-zlightgray bg-white'>
-              <Rightbarbtn text={"Get Report"} icon={<FaFileAlt style={btnstyle}/>}/>
-              <Rightbarbtn text={"Order Report"} icon={<FaUndo style={btnstyle}/>}/>
-              <Rightbarbtn text={"CSV Download"} icon={<FaDownload style={btnstyle}/>}/>
-          </div>
+  return (
+    <div className=" bg-white w-[32rem] h-full z-auto absolute right-0 bottom-0 lg:block md:hidden sm:hidden">
+      <div className="flex flex-row border-b-[1px] border-zlightgray justify-between items-center p-2 h-9">
+        <span className="text-purple">
+          <Newtab className="fill-purple h-3.5 w-3.5" />
+        </span>
+        <div className="flex flex-row">
+          <Bugreport className="h-5 w-5" />
+          <span className="text-gray font-light text-base">
+            <Cross className="h-5 w-5" />
+          </span>
+        </div>
       </div>
-    )
+      <div className="flex justify-between border-b-[1px] border-zlightgray px-5 pr-5">
+        <h1 className="text-xl text-zblue h-20 p-2 font-medium">
+          Los Angeles,CA,USA
+        </h1>
+        <span className="mt-5 mr-5">
+          <BookmarkIcon className="w-10 h-10 fill-zlightgray" />
+        </span>
+      </div>
+      <div className="flex w-full border-b-[1px] border-zlightgray p-4">
+        <Rightbartab text={"Zone Data"} />
+        <Rightbartab text={"Permitted use"} />
+        <Rightbartab text={"Controls"} />
+        <Rightbartab text={"Overlays"} />
+      </div>
+      <div className="h-[500px] overflow-auto p-5">
+        <ul className="leading-10 uppercase font-medium text-xs">
+          <li>Zone Code</li>
+          <li className="text-gray">C35</li>
+          <li>Zone name</li>
+          <ul>
+            <li className="text-gray">C4</li>
+          </ul>
+          <li>Zone type</li>
+          <ul>
+            <li className="text-gray">Comercial</li>
+          </ul>
+        </ul>
+      </div>
+      <div className="flex p-4 gap-1 bottom-0 absolute w-full border-t-[1px] border-zlightgray bg-white">
+        <Rightbarbtn
+          text={"Get Report"}
+          icon={<FaFileAlt style={btnstyle} />}
+        />
+        <Rightbarbtn text={"Order Report"} icon={<FaUndo style={btnstyle} />} />
+        <Rightbarbtn
+          text={"CSV Download"}
+          icon={<FaDownload style={btnstyle} />}
+        />
+      </div>
+    </div>
+  );
 }
 
-
-export default RightSidebar
+export default RightSidebar;
