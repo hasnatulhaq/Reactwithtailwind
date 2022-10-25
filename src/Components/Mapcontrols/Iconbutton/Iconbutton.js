153489c85b4f6f81bc2f12b65af18e467fb5fea3
hasnatulhaq <hasnatulhaq@outlook.com>
2022-10-25 17:53:03 +0500

    hide sidebar

--- src/Components/Mapcontrols/Iconbutton/Iconbutton.js
+++ src/Components/Mapcontrols/Iconbutton/Iconbutton.js
@@ -4,7 +4,7 @@ function Iconbutton({ icon, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="w-1/5 h-full rounded-l-md bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]"
+      className="w-1/5 h-full  bg-zmapmenu flex justify-center items-center pt-[0.5rem] pb-[0.5rem]"
     >
       {icon}
     </button>
